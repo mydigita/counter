25d1e1d4512aefa094e504990555d49b060ed59e
thetradecoder <thetradecoder@gmail.com>
2021-03-04 15:48:14 +0600

    revert local update

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { ThemeProvider, Button, Input } from 'react-native-elements';
+import {Container, Header, Title, Body, Footer, FooterTab, Content, Text, Button, Form, Item, Input, Label,  Grid, Row, Col, H1} from 'native-base';
 
 
 export default function App(){
@@ -21,22 +21,50 @@ export default function App(){
         setCountVar(Number(e.target.value))
     }
     return (
-        <ThemeProvider>
-            <h1>{count}</h1>
-            <Button bordered rounded  large onPress={onPressDec}><Text uppercase={false}>Dec. ( - )</Text></Button>
-                       
-            <Button bordered rounded  large onPress={onPressInc}><Text uppercase={false}>Inc. ( + )</Text></Button>
-                
+        <Container>
+
+          <Header style= {{backgroundColor:'#008080'}}>      
+            <Body >
+              <Title style= {{ alignSelf: 'center'}}>Counter App</Title>
+            </Body>
+          </Header>
+            <Content padder>
+                <Body>
+                <H1>{count}</H1>
+                </Body>
+                <Grid>
+                    <Row>
+                        <Col>
+                        <Body>
+                            <Button bordered rounded  large onPress={onPressDec}><Text uppercase={false}>Dec. ( - )</Text></Button>
+                        </Body>
+                        </Col>
+                        <Col>
+                        <Body>
+                            <Button bordered rounded  large onPress={onPressInc}><Text uppercase={false}>Inc. ( + )</Text></Button>
+                        </Body>
+                        </Col>
+                    </Row>
+                </Grid>
           
-            <Button block danger onPress={onPressResetCount}><Text uppercase={false}>Reset Result</Text></Button>
+                  <Button block danger onPress={onPressResetCount}><Text uppercase={false}>Reset Result</Text></Button>
           
-               
-            <Input keyboardType="numeric" placeholder='1' maxLength={12} onChange={onChangeCountVar}/>                       
-        
-            <Button block danger onPress={onPressResetCountVar}><Text uppercase={false}>Reset Variation</Text></Button>           
-          
-        </ThemeProvider>
+                <Form>
+                    <Item stackedLabel>
+                        <Label>Increase / decrease by</Label>
+                        <Input keyboardType="numeric" placeholder='1' maxLength={12} onChange={onChangeCountVar}/>                       
+                    </Item>
+                </Form>
+                <Button block danger onPress={onPressResetCountVar}><Text uppercase={false}>Reset Variation</Text></Button>           
+            </Content>
+
+            <Footer style={{backgroundColor:'#00182d'}}>
+              <FooterTab>
+                <Body>
+                <Text style={{color:'silver'}}>By: Mamun @ TradeCoder</Text>
+                </Body>
+              </FooterTab>
+            </Footer>
+        </Container>
     );
 }
-
-  
