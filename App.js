62e75f1d710e31125c7ac51bc2ab1b55f0465b4a
thetradecoder <thetradecoder@gmail.com>
2021-03-05 16:03:35 +0600

    using react native elements ui instead of nativebase

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
-import {Text} from 'react-native';
-import {Container, Header, Body, Footer, FooterTab, Content, Button, Form, Item, Input, Label,  Grid, Row, Col, H1} from 'native-base';
+import { ThemeProvider, Header, Text, Button, Input } from 'react-native-elements';
+import { Col, Row, Grid } from "react-native-easy-grid";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 
 
@@ -25,50 +26,25 @@ export default function App(){
 
     
     return (
-        <Container>
-
-          <Header style= {{backgroundColor:'#008080'}}>      
-            <Body >
-              <Text style= {{ alignSelf: 'center'}}>Counter App</Text>
-            </Body>
-          </Header>
-            <Content padder>
-                <Body>
-                <H1>{count}</H1>
-                </Body>
-                <Grid>
-                    <Row>
-                        <Col>
-                        <Body>
-                            <Button bordered rounded  large onPress={onPressDec}><Text uppercase={false}>Dec. ( - )</Text></Button>
-                        </Body>
-                        </Col>
-                        <Col>
-                        <Body>
-                            <Button bordered rounded  large onPress={onPressInc}><Text uppercase={false}>Inc. ( + )</Text></Button>
-                        </Body>
-                        </Col>
-                    </Row>
-                </Grid>
-          
-                  <Button block danger onPress={onPressResetCount}><Text uppercase={false}>Reset Result</Text></Button>
-          
-                <Form>
-                    <Item stackedLabel>
-                        <Label>Increase / decrease by</Label>
-                        <Input keyboardType="numeric" placeholder='1' maxLength={12} onChange={onChangeCountVar}/>                       
-                    </Item>
-                </Form>
-                <Button block danger onPress={onPressResetCountVar}><Text uppercase={false}>Reset Variation</Text></Button>           
-            </Content>
-
-            <Footer style={{backgroundColor:'#00182d'}}>
-              <FooterTab>
-                <Body>
-                <Text style={{color:'silver'}}>By: Mamun @ TradeCoder</Text>
-                </Body>
-              </FooterTab>
-            </Footer>
-        </Container>
+        <SafeAreaProvider>
+            <Header
+                leftComponent={{ icon: 'menu', color: '#fff' }}
+                centerComponent={{ text: 'Counter App', style: { color: '#fff' } }}
+                rightComponent={{ icon: 'home', color: '#fff' }}
+            />
+            <Grid>
+                <Row style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+                    <Text h1>{count}</Text>
+                </Row>
+                <Row>
+                    <Col>
+                        <Button title="Dec (-)" onPress={onPressDec}/>
+                    </Col>
+                    <Col>
+                        <Button title="Inc (+)" onPress={onPressInc}/>
+                    </Col>
+                </Row>
+            </Grid>
+        </SafeAreaProvider>
     );
 }
