d8da51161d5ff9af1af24e2294c53826dcaac1b5
thetradecoder <thetradecoder@gmail.com>
2021-03-10 16:51:17 +0600

    local update

--- App.js
+++ App.js
@@ -32,40 +32,43 @@ export default function App(){
         <SafeAreaProvider>
    
             <ThemeProvider theme={theme}>
-            <Header                
-                centerComponent={{ text: 'Counter App', style: { color: 'white', fontSize:20 } }}
+            <Header               
+                centerComponent={{ text: 'Easy Counter App', style: { color: 'white', fontSize:20 } }}
                 rightComponent={{text:'1.0', style:{color:'white', fontSize:18}}}               
             />
             <Grid>
                 <Row style={{flex:1, justifyContent:'center', alignItems:'center'}}>
                     <Text h1>{count}</Text>
                 </Row>
-                <Row style={{padding:10}}>   
+                <Row style={{padding:10, maxHeight:80}}>                    
+                    <Col>
+                        <Button type='outline'  title="Reset Count" onPress={onPressResetCount}/>
+                    </Col>
+                </Row>
+                               
+                <Row style={{padding:10, maxHeight:120}}>   
                     <Col style={{marginRight:30}}>
-                        <Button type='solid' title="Dec (-)" onPress={onPressDec}/>
+                        <Button type='solid' title="Dec. (-)" onPress={onPressDec}/>
                     </Col>
                 
                     <Col style={{marginLeft:30}}>
-                        <Button type='solid' title="Inc (+)" onPress={onPressInc} />
+                        <Button type='solid' title="Inc. (+)" onPress={onPressInc} />
                     </Col>                            
                 </Row>
-                <Row style={{padding:10}}>                    
-                    <Col>
-                        <Button type='outline' title="Reset Count" onPress={onPressResetCount}/>
-                    </Col>
-                </Row>
+                
+                
                 <Row style={{padding:10}}>
                     <Col>
-                    <Text>Inc/Dec. by</Text>                    
+                    <Text style={{fontSize:16}}>Inc/Dec. by</Text>                    
                        <Input defaultValue={countVar.toString()} autoCorrect={false} style={{fontSize:30, color:'red' }} keyboardType="numeric" onChangeText={onChangeCountVar}/>
                     </Col>
                     <Col>
                        <Button type='outline' title="Reset" onPress={onPressResetCountVar}/>                     
-                    </Col>                    
+                    </Col> 
                 </Row>
                 
                 <Row style={{justifyContent:'center', alignItems:'center', backgroundColor:'#2171EE', height:60}}>
-                    <Text style={{color:'white', fontSize:20}}>By: Mamun | TradeCoder</Text>
+                    <Text style={{color:'white', fontSize:20}}>Dev: Mamun @ TradeCoder</Text>
                 </Row>
             </Grid>
             </ThemeProvider>
